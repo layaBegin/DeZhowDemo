b029d71ba1b21178fc4c37eee3c0c85478ce2c3d
yunteng <maweiphilippines@gmail.com>
2021-11-22 16:31:25 +0800

    德州扑克demo 初版ok

--- Assets/Resources/Scripts/view/TestView.cs
+++ Assets/Resources/Scripts/view/TestView.cs
@@ -19,6 +19,10 @@ public class TestView : MonoBehaviour {
     public Text text2Type;
     public Text textWhoWin;
 
+    public Transform pos1;
+    public Transform pos2;
+    public Transform posCommon;
+
     private Color colorRed = new Color(255,0,0);
     private Color colorBlack = new Color(0,0,0);
 
@@ -40,19 +44,35 @@ public class TestView : MonoBehaviour {
     void onBtnAgain()
     {
         Debug.Log("===进入Btn回调");
-        this._setCardActice(false);
-        Invoke("DelayFunc", 1);
+        this._setCardActice(true);
+        //Invoke("DelayFunc", 1);
+        StartCoroutine(ShowA());
+        Debug.Log("After StartCoroutine");
     }
+
+    private IEnumerator ShowA()
+    {
+        yield return new WaitForSeconds(1);
+        DelayFunc();
+    }
+
 
     void _setCardActice(bool active)
     {
-        this.cardItems1[0].transform.parent.gameObject.SetActive(active);
-        this.cardItems2[0].transform.parent.gameObject.SetActive(active);
-        this.cardItemsCommon[0].transform.parent.gameObject.SetActive(active);
+        cardItems1[0].transform.gameObject.SetActive(active);
+        cardItems1[1].transform.gameObject.SetActive(active);
+        cardItems2[0].transform.gameObject.SetActive(active);
+        cardItems2[1].transform.gameObject.SetActive(active);
+        cardItems1[0].transform.gameObject.SetActive(active);
+        for (int i = 0; i < cardItemsCommon.Length; i++)
+        {
+            cardItemsCommon[i].transform.gameObject.SetActive(active);
+        }
     }
 
     void DelayFunc()
     {
+        this._setCardActice(true);
         int[] cardList = DZGameLogic.Instance.getRandCardList();
 
         int[] cardDataCommon = new int[] { cardList[0], cardList[1], cardList[2], cardList[3], cardList[4] };
@@ -60,15 +80,14 @@ public class TestView : MonoBehaviour {
         int[] cardData2 = new int[] { cardList[7], cardList[8] };
 
         updateCardItem(cardItems1[0], cardData1[0]);
-        //updateCardItem(cardItems1[1], cardData1[1]);
-        //updateCardItem(cardItems2[0], cardData2[0]);
-        //updateCardItem(cardItems2[1], cardData2[1]);
-        //for (int i = 0;i < cardItemsCommon.Length;i++)
-        //{
-        //    updateCardItem(cardItemsCommon[i],cardDataCommon[i]);
-        //}
+        updateCardItem(cardItems1[1], cardData1[1]);
+        updateCardItem(cardItems2[0], cardData2[0]);
+        updateCardItem(cardItems2[1], cardData2[1]);
+        for (int i = 0; i < cardItemsCommon.Length; i++)
+        {
+            updateCardItem(cardItemsCommon[i], cardDataCommon[i]);
+        }
 
-        this._setCardActice(true);
 
         //牌型，谁赢了 
         List<int> fiveCardData1= DZGameLogic.Instance.fiveFromSeven(new List<int>(cardData1), new List<int>(cardDataCommon));
@@ -101,6 +120,7 @@ public class TestView : MonoBehaviour {
         int colorNum = DZGameLogic.Instance.getCardColor(data) / 16;
         Sprite colorSprire = colors[colorNum];
         Color color = (colorNum % 2 == 0 ? colorBlack : colorRed);
+        Debug.Log("==numSprire:" + numSprire);
         cardItem.UpdateItem(numSprire, colorSprire, color);
     }
     
